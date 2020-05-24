2f73069b5cf24062eb475f142e1a4f4c346f7533
yuzu <s.yuzu441@gmail.com>
2020-05-24 15:05:16 +0900

    Add validaiton

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+const moduleName = pkg.name.replace(/^\@.*\//, '')
+const banner = `/*!
+${moduleName}.js v${pkg.version}
+${pkg.homepage}
+Released under the ${pkg.license} License.
+*/`
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: 'inline',
+      banner,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: 'inline',
+      banner
+    },
+  ],
+  plugins: [
+    typescript(),
+  ],
+}
---
+++ src/IsULID.ts
@@ -0,0 +1,24 @@
+import {buildMessage, ValidateBy, ValidationOptions} from 'class-validator'
+
+export const IS_ULID = 'isUlid'
+
+const pattern = /[0-9A-Z]{26}/
+
+export function isULID(value: unknown): boolean {
+  return typeof value === 'string' && pattern.test(value)
+}
+
+export function IsULID(
+  validationOptions?: ValidationOptions
+): PropertyDecorator {
+  return ValidateBy({
+    name: IS_ULID,
+    validator: {
+      validate: (value: string): boolean => isULID(value),
+      defaultMessage: buildMessage(
+        (eachPrefix) => eachPrefix + '$property must be an ULID',
+        validationOptions
+      ),
+    },
+  })
+}
