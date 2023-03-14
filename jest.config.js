fab0ef1bd5dccf588567296f64ed33612edd3105
yuzu <yuzu441@users.noreply.github.com>
2023-03-14 22:19:35 +0900

    Fix update dependency (#64)
    * chore: update depenendencies
    
    * fix: update rollup v3
    
    * fix: test error
    
    * feat: add tsc check to actions
    
    * fix: drop test 14.x

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-}
+/** @type {import('@jest/types').Config.InitialOptions} */
+export default ({
+  preset: 'ts-jest/presets/default-esm',
+  // testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+})
