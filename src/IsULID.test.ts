536c8c4a54e57580d9921f9fbcb3ce3b44960ecf
yuzu <yuzu441@users.noreply.github.com>
2023-08-05 21:26:44 +0900

    Fix validation length (#88)

--- src/IsULID.test.ts
+++ src/IsULID.test.ts
@@ -3,26 +3,31 @@ import { IsULID, isULID } from './IsULID'
 
 describe('isULID', () => {
   it('validate ulid', () => {
-    expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSS')).toBeTruthy()
+    expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSS')).toBe(true)
     // uuid v4
-    expect(isULID('23bc826e-e366-403e-bbe8-e28cdb960584')).toBeFalsy()
+    expect(isULID('23bc826e-e366-403e-bbe8-e28cdb960584')).toBe(false)
+  })
+
+  it('ulid length', () => {
+    // The id has one extra character
+    expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSSS')).toBe(false)
   })
 
   describe('Excluded charactors', () => {
     it('include "I"', () => {
-      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSI')).toBeFalsy()
+      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSI')).toBe(false)
     })
 
     it('include "L"', () => {
-      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSL')).toBeFalsy()
+      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSL')).toBe(false)
     })
 
     it('include "O"', () => {
-      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSO')).toBeFalsy()
+      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSO')).toBe(false)
     })
 
     it('include "U"', () => {
-      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSU')).toBeFalsy()
+      expect(isULID('01ED8ABHD21XTZJMRW6ZMEGPSU')).toBe(false)
     })
   })
 
